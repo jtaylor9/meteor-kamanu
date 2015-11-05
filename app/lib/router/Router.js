f37a4d2aeb6d6e2bc47a5470c07cca9b625d3b27
JOSHUA TAYLOR <joshuatlr418@gmail.com>
2015-11-05 10:13:27 -1000

    Finished extra credit pages

--- app/lib/router/Router.js
+++ app/lib/router/Router.js
@@ -1,15 +1,9 @@
-/**
- * Configure Iron Router.
- * See: http://iron-meteor.github.io/iron-router/
- */
-
 
 Router.configure({
   layoutTemplate: 'Layout',
   waitOn: function() { return Meteor.subscribe("Stuff"); },
   loadingTemplate: 'Loading'
 });
-
 Router.route('/', {
   name: 'Home'
 });
@@ -22,6 +16,21 @@ Router.route('/add', {
   name: 'AddStuff'
 });
 
+Router.route('/pueo', {
+  name: 'pueo'
+});
+
+Router.route('/aukahi', {
+  name: 'aukahi'
+});
+
+Router.route('/kamanuv6', {
+  name: 'kamanuv6'
+});
+
+Router.route('/about', {
+  name: 'about'
+});
 
 Router.route('/stuff/:_id', {
   name: 'EditStuff',
